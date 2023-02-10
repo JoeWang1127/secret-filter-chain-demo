c197eaa79ec3f912befd16466e0a17f42291270f
Joe Wang <joewa@google.com>
2023-02-09 19:48:03 -0500

    setup spring boot 3

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ In the `main` branch
 * Run `curl localhost:8080`, the terminal will return `No secrets here!`
 
 ### Running with Spring Boot 3.0.2
+In the `spring-boot3` branch
+
+* Start up the application
+* Run `curl localhost:8080`, nothing returns to the terminal. It seems that
+the `GetMapping` is not called.
--- src/main/java/com/example/securityfilterchain/SecurityConfiguration.java
+++ src/main/java/com/example/securityfilterchain/SecurityConfiguration.java
@@ -44,7 +44,7 @@ public class SecurityConfiguration {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests(authz -> authz.antMatchers("/topsecret").authenticated())
+    http.authorizeHttpRequests(authz -> authz.requestMatchers("/topsecret").authenticated())
         .oauth2ResourceServer()
         .jwt()
         .and()
